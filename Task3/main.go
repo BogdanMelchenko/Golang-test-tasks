191c9c71f5962a38b347a832220ba89a42db6c6b
Bogdan Melchenko <44803326+BogdanMelchenko@users.noreply.github.com>
2018-11-06 19:18:26 +0300

    hotfix 1
    fixed some bugs in task5
    made task3 look better

--- Task3/main.go
+++ Task3/main.go
@@ -7,7 +7,7 @@ import (
 
 type Computer interface {
 	addRAM(int)
-	toString() string
+	showRAM() string
 }
 
 type PC struct {
@@ -35,33 +35,29 @@ func main() {
 	var container ComputerContainer
 	for i := 0; i < 5; i++ {
 		if i%2 == 1 {
-			inside := PC{name: "desktop" + strconv.Itoa(i+1), ram: (i + 1) * 1024, cpu: "intel " + strconv.Itoa(i+2) + "86DX",
-				gpu: "AMD" + strconv.Itoa(i+1000) + "HD", cost: (i + 1) + (i+1)*300, storage: (i+1)*1000 + i*2*8*16}
-			container.computers[i] = Laptop{brand: "Brand" + strconv.Itoa(i), isBacklit: true, screen: 15.6, weight: 15.0*float32(i) + 1000.0, inner: inside}
-
+			container.computers[i] = new(Laptop)
 		} else {
-			container.computers[i] = PC{name: "desktop" + strconv.Itoa(i+1), ram: (i + 1) * 1024, cpu: "intel " + strconv.Itoa(i+2) + "86DX",
-				gpu: "AMD" + strconv.Itoa(i+1000) + "HD", cost: (i + 1) + (i+1)*300, storage: (i+1)*1000 + i*2*8*16}
+			container.computers[i] = new(PC)
 		}
 	}
 	for i, v := range container.computers {
-		fmt.Println(strconv.Itoa(i) + " | " + v.toString())
+		v.addRAM(512 * i)
+		fmt.Println(strconv.Itoa(i) + " | " + v.showRAM())
 	}
 }
 
-func (object PC) addRAM(additionRAM int) {
+func (object *PC) addRAM(additionRAM int) {
 	object.ram += additionRAM
 }
 
-func (object PC) toString() string {
-	return fmt.Sprintf("PC!\t\t name: %v, cpu: %v, gpu: %v, storage: %v, ram: %v, cost: %v", object.name, object.cpu, object.gpu, object.storage, object.ram, object.cost)
+func (object PC) showRAM() string {
+	return fmt.Sprintf("PC!\t\t  ram: %v", object.ram)
 }
 
-func (object Laptop) addRAM(additionRAM int) {
+func (object *Laptop) addRAM(additionRAM int) {
 	object.inner.ram += additionRAM
 }
 
-func (object Laptop) toString() string {
-	return fmt.Sprintf("Laptop!\t brand: %v, weight: %v, screen: %v, isBacklit: %v, name: %v, cost: %v",
-		object.brand, object.weight, object.screen, object.isBacklit, object.inner.name, object.inner.cost)
+func (object Laptop) showRAM() string {
+	return fmt.Sprintf("Laptop!\t ram: %v", object.inner.ram)
 }
--- Task5/main.go
+++ Task5/main.go
@@ -4,61 +4,54 @@ import (
 	"flag"
 	"fmt"
 	"math"
-	"net"
 	"net/http"
 	"sort"
 	"time"
 )
 
-func ping(index int, adress string, client http.Client, resultChannel chan float64) {
-	resp, err := client.Get(adress)
-	if err != nil && net.Error.Timeout(err.(net.Error)) {
+// Ping returns request duration or -1.0 (float64) if gets Timeout error
+func Ping(adress string, client http.Client, resultChannel chan float64) {
+	startTime := time.Now()
+	response, err := client.Get(adress)
+	if err != nil {
 		resultChannel <- -1.0
 		return
 	}
-	start := time.Now()
-	var duration float64
-	defer resp.Body.Close()
-	for {
-		bs := make([]byte, 1014)
-		_, err := resp.Body.Read(bs)
-		if err != nil {
-			finish := time.Now()
-			elapsed := finish.Sub(start)
-			duration = float64(elapsed) / float64(time.Millisecond)
-			resultChannel <- duration
-			break
-		}
-	}
+	var durationInFloat float64
+	defer response.Body.Close()
+	elapsedOnRequest := time.Now().Sub(startTime)
+	durationInFloat = float64(elapsedOnRequest) / float64(time.Millisecond)
+	resultChannel <- durationInFloat
 }
 
 func main() {
-
-	quantity := flag.Int("quantity", 10, "quantity of requests")
-	var address string
-	flag.StringVar(&address, "address", "https://google.com", "where to send requests")
-	timeout := flag.Int("timeout", 6, "timeout of request")
+	requestQuantity := flag.Int("quantity", 10, "quantity of requests")
+	var requestAddress string
+	flag.StringVar(&requestAddress, "address", "https://google.com", "where to send requests")
+	timeout := flag.Int("timeout", int(1000), "timeout of request in ms")
 	flag.Parse()
-
+	fmt.Println("request adress: ", requestAddress, "|\tquantity of requests: ", *requestQuantity, "|\trequest timeout", *timeout, "ms")
 	resultChannel := make(chan float64)
-	timeoutDuration := time.Duration(*timeout)
+
+	i := time.Duration(*timeout)
+	timeoutDuration := time.Duration(i * time.Millisecond)
 	client := http.Client{
-		Timeout: timeoutDuration * time.Second,
+		Timeout: timeoutDuration,
 	}
 
 	startTime := time.Now()
-	for i := 0; i < *quantity; {
-		go ping(i, address, client, resultChannel)
+	for i := 0; i < *requestQuantity; {
+		go Ping(requestAddress, client, resultChannel)
 		i++
 	}
-	i := 0
-	timeouts := 0
-	results := make([]float64, *quantity)
-	for i < *quantity {
+
+	timeoutsQuantity := 0
+	results := make([]float64, *requestQuantity)
+	for i := 0; i < *requestQuantity; {
 		select {
 		case result := <-resultChannel:
 			if result < float64(0.0) {
-				timeouts++
+				timeoutsQuantity++
 			}
 			results[i] = result
 			i++
@@ -70,16 +63,20 @@ func main() {
 
 	filteredResults := results[:0]
 	for _, res := range results {
-		if res > 0.0 {
+		if res > -1.0 {
 			filteredResults = append(filteredResults, res)
 		}
 	}
-	sort.Float64s(filteredResults)
 
-	min := filteredResults[0]
-	max := filteredResults[len(filteredResults)-1]
-
-	printResults(address, min, max, timeouts, *quantity, totalTime)
+	if len(filteredResults) > 0 {
+		sort.Float64s(filteredResults)
+		minResponseTime := filteredResults[0]
+		maxResponseTime := filteredResults[len(filteredResults)-1]
+		averageResponseTime := calculateAverage(filteredResults)
+		printResults(requestAddress, minResponseTime, maxResponseTime, averageResponseTime, timeoutsQuantity, *requestQuantity, totalTime)
+	} else {
+		fmt.Println("no response, try changing request timeout value")
+	}
 	fmt.Scanln()
 }
 
@@ -87,19 +84,29 @@ func round(num float64) int {
 	return int(num + math.Copysign(0.5, num))
 }
 
-func toFloat64Milliseconds(goal time.Duration) float64 {
+// DurationToFloat64 makes float value from time.duration (returns float64 value of milliseconds)
+func DurationToFloat64(goal time.Duration) float64 {
 	return float64(goal) / float64(time.Millisecond)
 }
 
+func calculateAverage(target []float64) float64 {
+	sum := 0.0
+	for _, value := range target {
+		sum += value
+	}
+	return sum / float64(len(target))
+}
+
+// toFixed truncates float value to given precision
 func toFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(round(num*output)) / output
 }
 
-func printResults(address string, min, max float64, timeouts, quantity int, totalTime time.Duration) {
-	fmt.Println("total time for", address, "is:", toFixed(toFloat64Milliseconds(totalTime), 2), "ms")
-	fmt.Println("average is: ", toFixed(toFloat64Milliseconds(totalTime)/float64(quantity), 2), "ms")
-	fmt.Println("request timeouts / total requests:", timeouts, "/", quantity)
-	fmt.Println("min is:", toFixed(min, 2), "ms")
-	fmt.Println("max is:", toFixed(max, 2), "ms")
+func printResults(requestAddress string, minResponseTime, maxResponseTime, averageResponseTime float64, timeoutsQuantity, requestQuantity int, totalTime time.Duration) {
+	fmt.Println("total time for", requestAddress, "is:", toFixed(DurationToFloat64(totalTime), 2), "ms")
+	fmt.Println("average is: ", toFixed(averageResponseTime, 2), "ms")
+	fmt.Println("request timeouts / total requests:", timeoutsQuantity, "/", requestQuantity)
+	fmt.Println("min is:", toFixed(minResponseTime, 2), "ms")
+	fmt.Println("max is:", toFixed(maxResponseTime, 2), "ms")
 }
